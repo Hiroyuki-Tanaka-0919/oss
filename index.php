d516f7dd106610583ccbe3864d964e0c724d987b
Hiroyuki Tanaka <h.tanaka.tisdx.2024@gmail.com>
2026-08-03 10:51:54 +0900

    Add copyright

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>OSS置き場(仮)</title>
+</head>
+<body>
+  <h1 id="oss置き場仮">OSS置き場(仮)</h1>
+  <hr>
+  <ul>
+    <li><a href="https://pypi.org/project/pyfishsay/">pyfishsay</a> - Pythonで作った魚が喋るやつ（PYPI）</li>
+    <li><a href="http://ht0919.s323.xrea.com/lifespan/index.php">lifespan</a> - PHPで作った余命を計算するWebアプリ</li>
+  </ul>
+  <hr>
+  <center>Copyright © 2026 Hiroyuki Tanaka.</center>
+</body>
+</html>
